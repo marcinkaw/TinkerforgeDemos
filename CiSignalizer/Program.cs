789b8bcc3e34254b7d632317ccc9211a60bc6533
Marcin Kawalerowicz <marcin@kawalerowicz.net>
2014-04-15 13:24:51 +0200

    Settings in CI Sygnalizer

--- CiSignalizer/Program.cs
+++ CiSignalizer/Program.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Threading;
 using System.Xml.Linq;
 using Tinkerforge;
+using System.Text;
 #endregion
 
 namespace TinkerForgeProjects.CiSignalizer
@@ -24,32 +25,44 @@ namespace TinkerForgeProjects.CiSignalizer
 
 		#region Fields
 		// The host and the port to the master brick
-		private static string _Host = "localhost";
+		private static string _Host = MySettings.Default.TinkerForgeIp;
+		//private static string _Host = "localhost";
 		private static int _Port = 4223;
 		// The UID of the Dual Relay Bricklet
 		private static string _BrickletDualRelayUid = "a7E";
-		private static string _BrickletIO4Uid = "aej";
+		private static string _BrickleLedUid = "eAy";
 		// The jenkins xml api request filter
-		//private static string _XmlApiRequest = "http://ci1:8181/api/xml?tree=jobs[lastBuild[building,result]]";
-		private static string _XmlApiRequest = "http://ci1:8181/view/Test/api/xml?tree=jobs[lastBuild[building,result]]";
+		//private static string _XmlApiRequest = "http://ci1:8181/view/Test/api/xml?tree=jobs[name,lastBuild[building,result]]";
+		private static string _XmlApiRequest = MySettings.Default.JenkinsURL;
 		#endregion
 
 		#region Main
 		static void Main(string[] args)
 		{
+			bool WasSignaled = false;
 			IPConnection ipConnection = new IPConnection();
 
 			BrickletDualRelay brickletDualRelay = new BrickletDualRelay(_BrickletDualRelayUid, ipConnection);
+			BrickletLCD20x4 brickledLed = new BrickletLCD20x4(_BrickleLedUid, ipConnection);
 
-			BrickletIO4 brickletIO4 = new BrickletIO4(_BrickletIO4Uid, ipConnection);
-
-			ipConnection.Connect(_Host, _Port);
+			try
+			{
+				ipConnection.Connect(_Host, _Port);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				Console.ReadKey();
+				return;
+			}
 
 			Console.WriteLine("Press any key to exit");
 
 			do
 			{
 				TrafficLightStates trafficLightState = TrafficLightStates.Off;
+				string[] ledTexts = new string[4];
+
 				try
 				{
 					using (WebClient webClient = new WebClient())
@@ -60,7 +73,7 @@ namespace TinkerForgeProjects.CiSignalizer
 						IEnumerable<XElement> lastBuildElements = rootElement
 							.Elements("job")
 							.Select(item => item.Element("lastBuild"))
-							.Where(item => (item != null)); ;
+							.Where(item => (item != null));
 
 						// Build server state: Success
 						trafficLightState = TrafficLightStates.Green;
@@ -86,26 +99,100 @@ namespace TinkerForgeProjects.CiSignalizer
 							// Build server state: Failure
 							trafficLightState = TrafficLightStates.Red;
 						}
+
+						int AllCount = lastBuildElements.Count();
+						int IsBuldingCount = lastBuildElements
+							.Select(item => item.Element("building"))
+							.Where(item => (item != null))
+							.Select(item => item.Value)
+							.Where(item => (!string.IsNullOrEmpty(item)))
+							.Select(item => item.ToUpperInvariant())
+							.Where(item => (item == "TRUE")).Count();
+						int FailuteCount = lastBuildElements
+							.Select(item => item.Element("result"))
+							.Where(item => (item != null))
+							.Select(item => item.Value)
+							.Where(item => (!string.IsNullOrEmpty(item)))
+							.Select(item => item.ToUpperInvariant())
+							.Where(item => (item == "FAILURE")).Count();
+						int SuccessCount = lastBuildElements
+							.Select(item => item.Element("result"))
+							.Where(item => (item != null))
+							.Select(item => item.Value)
+							.Where(item => (!string.IsNullOrEmpty(item)))
+							.Select(item => item.ToUpperInvariant())
+							.Where(item => (item == "SUCCESS")).Count();
+
+						ledTexts[0] = "Jobs: " + AllCount.ToString();
+						ledTexts[1] = "Failures: " + FailuteCount.ToString();
+						ledTexts[2] = "Succeeded: " + SuccessCount.ToString();
+						ledTexts[3] = "Building: " + IsBuldingCount.ToString();
+
+						
 					}
 				}
 				catch
 				{
+					continue;
 					// Ignore
 				}
 
 				System.Console.WriteLine(trafficLightState.ToString());
+
+
 				if ((trafficLightState != TrafficLightStates.Off) && (ipConnection.GetConnectionState() == 1))
 				{
-					bool relay1 = false;
-					bool relay2 = false;
-					// Set dual realy bricklet
-					MyGetRelayStatesFromTrafficLightState(trafficLightState, out relay1, out relay2);
-					brickletDualRelay.SetState(relay1, relay2);
-					// Set IO4 bricklet
-					MySetBrickletIO4State(trafficLightState, brickletIO4);
+					//bool relay1 = false;
+					//bool relay2 = false;
+					//MyGetRelayStatesFromTrafficLightState(trafficLightState, out relay1, out relay2);
+					//brickletDualRelay.SetState(relay1, relay2);
+
+
+					if (trafficLightState == TrafficLightStates.Red)
+					{
+						
+						if (!WasSignaled)
+						{
+							Console.WriteLine("turnOn");
+							brickletDualRelay.SetState(true, true);
+							
+							Thread.Sleep(10000);
+							Console.WriteLine("turnOff");
+							brickletDualRelay.SetState(false, false);
+						}
+						WasSignaled = true;
+					}
+					else
+					{
+						WasSignaled = false;
+						Console.WriteLine("turnOff");
+						brickletDualRelay.SetState(false, false);
+					}
 				}
 
-				
+				if (ipConnection.GetConnectionState() == 1 && ledTexts != null)
+				{
+						brickledLed.ClearDisplay();
+						brickledLed.WriteLine(0, 0, ledTexts[0]);
+						brickledLed.WriteLine(1, 0, ledTexts[1]);
+						brickledLed.WriteLine(2, 0, ledTexts[2]);
+						brickledLed.WriteLine(3, 0, ledTexts[3]);
+						if (trafficLightState == TrafficLightStates.Red)
+						{
+							if (brickledLed.IsBacklightOn())
+							{
+								brickledLed.BacklightOff();
+							}
+							else
+							{
+								brickledLed.BacklightOn();
+							}
+						}
+						else
+						{
+							brickledLed.BacklightOff();
+						}
+				}
 
 				Thread.Sleep(1000);
 			}
@@ -113,43 +200,9 @@ namespace TinkerForgeProjects.CiSignalizer
 
 			ipConnection.Disconnect();
 		}
-
 		#endregion
 
 		#region My Methods
-		private static void MySetBrickletIO4State(TrafficLightStates trafficLightState, BrickletIO4 brickletIO4)
-		{
-			switch (trafficLightState)
-			{
-				case TrafficLightStates.Off:
-					brickletIO4.SetConfiguration(1 << 0, 'o', false);
-					brickletIO4.SetConfiguration(1 << 1, 'o', false);
-					brickletIO4.SetConfiguration(1 << 2, 'o', false);
-					brickletIO4.SetConfiguration(1 << 3, 'o', false);
-					break;
-				case TrafficLightStates.Red:
-					brickletIO4.SetConfiguration(1 << 0, 'o', true);
-					brickletIO4.SetConfiguration(1 << 1, 'o', true);
-					brickletIO4.SetConfiguration(1 << 2, 'o', false);
-					brickletIO4.SetConfiguration(1 << 3, 'o', false);
-					break;
-				case TrafficLightStates.Yellow:
-					brickletIO4.SetConfiguration(1 << 0, 'o', false);
-					brickletIO4.SetConfiguration(1 << 1, 'o', false);
-					brickletIO4.SetConfiguration(1 << 2, 'o', true);
-					brickletIO4.SetConfiguration(1 << 3, 'o', false);
-					break;
-				case TrafficLightStates.Green:
-					brickletIO4.SetConfiguration(1 << 0, 'o', false);
-					brickletIO4.SetConfiguration(1 << 1, 'o', false);
-					brickletIO4.SetConfiguration(1 << 2, 'o', false);
-					brickletIO4.SetConfiguration(1 << 3, 'o', true);
-					break;
-				default:
-					break;
-			}
-		}
-
 		private static void MyGetRelayStatesFromTrafficLightState(TrafficLightStates trafficLightState, out bool relay1, out bool relay2)
 		{
 			// Yellow
